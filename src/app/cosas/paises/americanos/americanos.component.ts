c0c94f6984b42cc58c2ab47020dfe1d9d3cb4afa
Anastasia <a.z.supahakka@gmail.com>
2021-12-15 08:01:53 +0100

    Codigo optimizado

--- src/app/cosas/paises/americanos/americanos.component.ts
+++ src/app/cosas/paises/americanos/americanos.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AmericanosComponent implements OnInit {
 
+  americanos: string[] = [
+    'Estados Unidos',
+    'Canada',
+    'Mexico',
+    'Colombia',
+    'Brasil',
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/cosas/paises/europeos/europeos.component.ts
+++ src/app/cosas/paises/europeos/europeos.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EuropeosComponent implements OnInit {
 
+  europeos: string[] = [
+    'España',
+    'Italia',
+    'Francia',
+    'Portugal',
+    'Alemania',
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
